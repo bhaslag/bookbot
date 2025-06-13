19544b4286e450b0845babcdc4819ad643336986
wihas <wihas@smile.fr>
2025-06-13 13:04:44 +0200

    Finished bookbot

--- main.py
+++ main.py
@@ -7,16 +7,26 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    txt = get_book_text('books/frankenstein.txt')
-
-    # num_words = count_words(txt)
-    # print(f"{num_words} words found in the document")
+    import sys
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    txt = get_book_text(sys.argv[1])
 
     num_letters = count_letters(txt)
     word_count = count_words(txt)
     sorted_letters = descending(num_letters)
-    # print(num_letters)
-    # print(word_count)
-    print(sorted_letters)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("----------- Character Count ----------")
+    for dictionary in sorted_letters:
+        if dictionary['char'].isalpha():
+            print(f"{dictionary['char']}: {dictionary['num']}")
+    print("============= END ===============")
 
 main()
